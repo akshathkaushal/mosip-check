6e52ad71eddcd3d07ca017905054d276233d9271
Akshath Kaushal <akshath.kaushal@gmail.com>
2022-11-08 14:50:31 +0530

    Added code

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM debian:buster
+RUN mkdir /app
+WORKDIR /app
+RUN apt update && \
+      apt install -y curl && \
+      curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+      chmod +x ./kubectl && \
+      mv ./kubectl /usr/local/bin/kubectl
+COPY ./ticket.sh .
+RUN pwd
+RUN ls -a
---
+++ ticket.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# part 1
+## get the names of all pods and namespaces into an array 
+namespace_arr=$(kubectl get deployments --all-namespaces | awk 'NR>1{print $1}')
+deployment_arr=$(kubectl get deployments --all-namespaces | awk 'NR>1{print $2}')
+readarray deployment_array <<<  $deployment_arr
+readarray namespace_array <<< $namespace_arr
+
+## length of the pod array == length of namespace array
+len=${!deployment_array[@]}
+
+for i in $len
+do 
+    echo -ne "Deployment: ${deployment_array[$i]}\r" 
+    echo -ne "Namespace: ${namespace_array[$i]}\r" 
+
+    kubectl describe pod -n ${namespace_array[$i]} ${deployment_array[$i]} | grep -i readiness
+    kubectl describe pod -n ${namespace_array[$i]} ${deployment_array[$i]} | grep -i liveness
+    kubectl describe pod -n ${namespace_array[$i]} ${deployment_array[$i]} | grep -i startup
+
+    echo ""
+done
+
+echo ""
+echo ""
+
+# part 2
+## get the names of all images in an array
+#images_array=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" | tr -s '[[:space:]]' '\n')
+images_array=$(kubectl get deployments --all-namespaces -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n')
+
+for image in $images_array
+do
+    echo "$image"
+done
+
+
+# part 3
+## install jq
+# apt-get update
+# apt-get install jq
+
+# for image in $images_array
+# do
+#     echo "$image"
+#     docker pull $image
+#     docker inspect $image | jq '.[0].ContainerConfig.Labels'
+#     docker rmi $image
+#     echo ""
+# done
