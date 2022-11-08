c9df65e6a68867a8d6feb3364b72635ac553addf
Akshath Kaushal <akshath.kaushal@gmail.com>
2022-11-09 01:02:47 +0530

    Added cron job

--- ticket.sh
+++ ticket.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Following are the readiness, liveness, and startup probes present in the deployments in the cluster:"
+echo ""
+
 # part 1
 ## get the names of all pods and namespaces into an array 
 namespace_arr=$(kubectl get deployments --all-namespaces | awk 'NR>1{print $1}')
@@ -29,6 +32,9 @@ echo ""
 ## get the names of all images in an array
 #images_array=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" | tr -s '[[:space:]]' '\n')
 images_array=$(kubectl get deployments --all-namespaces -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n')
+
+echo "Following are the images deployed in the cluster:"
+echo ""
 
 for image in $images_array
 do
